127b93bd89c3b3f96da5c2969e8e68e00d750b83
Piotr Ptak <piotr.a.ptak@capgemini.com>
2020-04-14 13:22:57 +0200

    Initial version of image fetcher

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"runtime"
+	"strings"
+	"sync"
 
 	"github.com/jessevdk/go-flags"
 )
@@ -13,12 +20,98 @@ type Options struct {
 
 func main() {
 	var options Options
-	_, err := flags.Parse(&options)
+	_, optionsErr := flags.Parse(&options)
 
-	if err != nil {
-		panic(err)
+	if optionsErr != nil {
+		panic(optionsErr)
 	}
 
 	fmt.Println(options)
 
+	urls, readingError := readLines(options.SourceFile)
+
+	if readingError != nil {
+		panic(readingError)
+	}
+
+	work := make(chan string)
+	shutdown := make(chan bool)
+	pool := runtime.NumCPU()
+	var wg sync.WaitGroup
+	wg.Add(pool)
+
+	for i := 0; i < pool; i++ {
+		go downloadUrls(work, shutdown, &wg, options.DestinationFolder)
+	}
+
+	for _, url := range urls {
+		work <- url
+	}
+
+	close(shutdown)
+
+	wg.Wait()
+}
+
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+func downloadUrls(urls chan string, shutdown chan bool, wg *sync.WaitGroup, downloadFolder string) {
+	for {
+		select {
+		case url := <-urls:
+			downloadUrl(url, downloadFolder)
+		case <-shutdown:
+			wg.Done()
+			return
+		}
+	}
+}
+
+func downloadUrl(url, downloadFolder string) {
+	fmt.Println("Downloading: ", url)
+
+	response, responseError := http.Get(url)
+
+	if responseError != nil {
+		fmt.Println("Error during downling", url, responseError.Error())
+	}
+
+	fmt.Println("finished")
+
+	defer response.Body.Close()
+
+	elements := []string{downloadFolder, buildFileName(url)}
+	fullFilePath := strings.Join(elements, "/")
+	file, fileError := os.Create(fullFilePath)
+
+	if fileError != nil {
+		fmt.Println("Error during creation of the file", fullFilePath, fileError.Error())
+	}
+
+	defer file.Close()
+
+	_, saveError := io.Copy(file, response.Body)
+
+	if saveError != nil {
+		fmt.Println("Error during saving image", fullFilePath, fileError.Error())
+	}
+}
+
+func buildFileName(url string) string {
+	segments := strings.Split(url, "/")
+
+	return segments[len(segments)-1]
 }
